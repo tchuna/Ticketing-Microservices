8fa931de58bf20725e1c1b651ab39654eb0458a5
elgner <eramos@euronext.com>
2022-08-02 12:34:20 +0100

    create auth service

---
+++ src/auth/node_modules/@google-cloud/bigtable/build/src/v2/index.d.ts
@@ -0,0 +1,3 @@
+export { BigtableInstanceAdminClient } from './bigtable_instance_admin_client';
+export { BigtableTableAdminClient } from './bigtable_table_admin_client';
+export { BigtableClient } from './bigtable_client';
